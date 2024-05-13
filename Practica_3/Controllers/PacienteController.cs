8ff9f55211d3a0a1684f9c24e1373d3edf69f135
Oscar Daniel Vargas <odanitocbba@gmail.com>
2024-05-13 18:53:18 -0400

    P3-001 Create Controller, Added post endpoint

---
+++ Practica_3/Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+﻿using BussinesLogic.Managers;
+using BussinesLogic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica_3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        private Manager manager;
+        public PacienteController(Manager manager )
+        { 
+            this.manager = manager;
+        }
+        [HttpPost]
+        public string Post([FromBody] Paciente paciente)
+        { 
+            return manager.CodigoPaciente(paciente); 
+        }
+    }
+}
