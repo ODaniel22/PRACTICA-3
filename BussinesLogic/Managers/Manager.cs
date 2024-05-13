b1442780d4b8a31b02db026f73fd878fa9c7e6a8
Oscar Daniel Vargas <odanitocbba@gmail.com>
2024-05-13 18:32:38 -0400

    P3-001 CREATE THE PROJECT AND CLASSLIBRARY, ADD PACIENTE AND MODEL

---
+++ BussinesLogic/Managers/Manager.cs
@@ -0,0 +1,19 @@
+﻿using BussinesLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Managers
+{
+    public class Manager
+    {
+        public string CodigoPaciente(Paciente paciente)
+        {
+            string codigo;
+            codigo = $"{paciente.nombre[0]}{paciente.apellido[0]}-{paciente.CI}";
+            return codigo;
+        }
+    }
+}
---
+++ BussinesLogic/Models/Paciente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Models
+{
+    public class Paciente
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int CI { get; set; }
+        public Paciente(string nombre, string apellido, int ci)
+        {
+            this.nombre =nombre;
+            this.apellido =apellido;
+            this.CI = ci;
+        }
+    }
+}
