ce7d68dce3a15c6908dfaa096917a1d1bd871824
Oscar Daniel Vargas <odanitocbba@gmail.com>
2024-05-13 20:23:52 -0400

    P3-001 Create the Middleware, tests and finish

---
+++ Practica_3/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Practica_3.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await processException(ex, httpContext);
+            }
+
+        }
+        private Task processException(Exception ex, HttpContext httpContext)
+        {
+            Log.Error(ex.Message);
+            return httpContext.Response.WriteAsJsonAsync(new { Code = (int)HttpStatusCode.InternalServerError, Error = ex.Message });
+        }
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
--- Practica_3/Program.cs
+++ Practica_3/Program.cs
@@ -1,4 +1,6 @@
 using BussinesLogic.Managers;
+using Practica_3.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<Manager>();
+Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("..\\Log-.log", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseExceptionMiddleware();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
